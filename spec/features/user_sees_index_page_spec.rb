cbefe885f4cd3aae60e70907e9e3c93d6059e6de
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-10-12 10:56:47 -0600

    finishes user story 2

--- spec/features/user_sees_index_page_spec.rb
+++ spec/features/user_sees_index_page_spec.rb
@@ -4,12 +4,20 @@ describe 'User visits /astronauts' do
 
   before(:each) do
     @astronaut_1 = Astronaut.create(name: "Neil Armstrong", age: 40, job: "Captain")
-    @astronaut_2 = Astronaut.create(name: "Buzz Alrdin", age: 42, job: "Engineer")
+    @mission_1 = @astronaut_1.missions.create(title: "Mission 1", time_in_space: 365)
+
+    @mission_2 = @astronaut_1.missions.create(title: "Mission 2", time_in_space: 180)
+    @astronaut_2 = @mission_1.astronauts.create(name: "Buzz Alrdin", age: 42, job: "Engineer")
   end
 
   it 'user sees astronaut name, age, and job' do
     visit '/astronauts'
 
     expect(page).to have_content(@astronaut_1.name)
+    expect(page).to have_content(@astronaut_1.age)
+    expect(page).to have_content(@astronaut_1.job)
+    expect(page).to have_content(@astronaut_2.name)
+    expect(page).to have_content(@astronaut_2.age)
+    expect(page).to have_content(@astronaut_2.job)
   end
 end
--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 describe Astronaut, type: :model do
+
+  before(:each) do
+    @astronaut_1 = Astronaut.create(name: "Neil Armstrong", age: 40, job: "Captain")
+    @mission_1 = @astronaut_1.missions.create(title: "Mission 1", time_in_space: 365)
+
+    @mission_2 = @astronaut_1.missions.create(title: "Mission 2", time_in_space: 180)
+    @astronaut_2 = @mission_1.astronauts.create(name: "Buzz Alrdin", age: 50, job: "Engineer")
+  end
+
   describe 'Validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :age }
@@ -10,5 +19,13 @@ describe Astronaut, type: :model do
   describe 'Relationships' do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
+  end
+
+  describe 'Model methods' do
+    it 'average_age' do
+      average = Astronaut.average_age
+
+      expect(average).to eq(45)
+    end
   end
 end
